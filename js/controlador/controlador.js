76f4d3256a529c7a300bae1c8af09cff83aca6e6
Cecilia Mermoz <42853540+cecimermoz@users.noreply.github.com>
2020-02-12 11:03:03 -0300

    Add files via upload

--- js/controlador/controlador.js
+++ js/controlador/controlador.js
@@ -2,17 +2,23 @@
  * Controlador
  */
 var Controlador = function(modelo) {
-  this.modelo = modelo;
+    this.modelo = modelo;
 };
 
 Controlador.prototype = {
-  agregarPregunta: function(pregunta, respuestas) {
-    this.modelo.agregarPregunta(pregunta, respuestas);
-  },
-  borrarPregunta: function(id){
-    this.modelo.borrarPregunta(id);
-  },
-  borrarTodo: function(){
-    this.modelo.borarTodo();
-  },
+    agregarPregunta: function(pregunta, respuestas) {
+        this.modelo.agregarPregunta(pregunta, respuestas);
+    },
+    borrarPregunta: function(id) {
+        this.modelo.borrarPregunta(id);
+    },
+    editarPregunta: function(id, nombre, respuestas) {
+        this.modelo.editarPregunta(id, nombre, respuestas);
+    },
+    borrarTodo: function() {
+        this.modelo.borrarTodo();
+    },
+    agregarVoto: function(id, respuestaSeleccionada) {
+        this.modelo.agregarVoto(id, respuestaSeleccionada);
+    }
 };
--- js/modelo/modelo.js
+++ js/modelo/modelo.js
@@ -2,60 +2,74 @@
  * Modelo
  */
 var Modelo = function() {
-  this.preguntas = [];
-  this.ultimoId = 0;
+    this.preguntas = [];
+    this.preguntasGuardadas = "";
+    this.ultimoId = 0;
 
-  //inicializacion de eventos
-  this.preguntaAgregada = new Evento(this);
-  this.preguntaBorrada = new Evento (this);
-  this.borrarTodo = new Evento (this);
+    //inicializacion de eventos
+    this.preguntaAgregada = new Evento(this);
+    this.preguntaEliminada = new Evento(this);
+    this.preguntaEditada = new Evento(this);
+    this.todasEliminadas = new Evento(this);
+    this.votoAgregado = new Evento(this);
+
 };
 
 Modelo.prototype = {
-  //se obtiene el id más grande asignado a una pregunta
-  obtenerUltimoId: function() {
-    if(this.preguntas.length == 0){
-      return 0;
-    } else {
-      var idAnterior = this.preguntas[0].id; 
-      for(var i = 0; i<this.preguntas.length;i++){
-        idActual = this.preguntas[i].id;
-        if(idActual >= idAnterior){
-          idAnterior = idActual;
+    //se obtiene el id más grande asignado a una pregunta
+    obtenerUltimoId: function() {
+        if (this.preguntas.length > 0) {
+            return this.preguntas[this.preguntas.length - 1].id
+        } else {
+            return 0;
         }
-      }
-      return idAnterior;
+
+    },
+
+
+    //se agrega una pregunta dado un nombre y sus respuestas
+    agregarPregunta: function(nombre, respuestas) {
+        var id = this.obtenerUltimoId();
+        id++;
+        var nuevaPregunta = { 'textoPregunta': nombre, 'id': id, 'cantidadPorRespuesta': respuestas };
+        this.preguntas.push(nuevaPregunta);
+        this.guardar();
+        this.preguntaAgregada.notificar();
+    },
+    borrarPregunta: function(id) {
+        this.preguntas = this.preguntas.filter(a => a.id !== id);
+        this.guardar();
+        this.preguntaEliminada.notificar();
+    },
+    // Primero agarrar la que quiero, luego usar lo de agregar pregunta
+    editarPregunta: function(id, nombre, respuestas) {
+        var a = this.preguntas.find(a => a.id == id);
+        a.textoPregunta = nombre;
+        a.cantidadPorRespuesta = respuestas;
+        this.guardar();
+        this.preguntaEditada.notificar();
+    },
+    borrarTodo: function() {
+        this.preguntas = [];
+        this.guardar();
+        this.todasEliminadas.notificar();
+    },
+
+    //se guardan las preguntas
+    guardar: function() {
+        this.preguntasGuardadas = localStorage.setItem("preguntas", JSON.stringify(this.preguntas));
+    },
+
+    recupero: function() {
+        var recuperadas = localStorage.getItem("preguntas");
+        this.preguntas = JSON.parse(recuperadas) || [];
+
+    },
+    agregarVoto: function(id, respuestaSeleccionada) {
+        var a = this.preguntas.find(a => a.id == id);
+        var b = a.cantidadPorRespuesta.find(b => b.textoRespuesta == respuestaSeleccionada);
+        b.cantidad++;
+        this.guardar();
+        this.votoAgregado.notificar();
     }
-  },
-
-  //se agrega una pregunta dado un nombre y sus respuestas
-  agregarPregunta: function(nombre, respuestas) {
-    var id = this.obtenerUltimoId();
-    id++;
-    var nuevaPregunta = {'textoPregunta': nombre, 'id': id, 'cantidadPorRespuesta': respuestas};
-    this.preguntas.push(nuevaPregunta);
-    this.guardar();
-    this.preguntaAgregada.notificar();
-  },
-
-  borrarPregunta: function(id){
-    var nueva = this.preguntas.filter(e => e.id !== id);
-    this.preguntas = nueva;
-    this.guardar();
-    this.preguntaBorrada.notificar();
-  },
-
-  borarTodo: function(){
-    var boton = $('#borrarTodo');
-    var miThis = this;
-    boton.click(function(){
-      miThis.preguntas.forEach(element => miThis.preguntas.pop());
-    });
-    this.guardar();
-    this.borrarTodo.notificar();
-  },
-
-  //se guardan las preguntas
-  guardar: function(){
-  },
-}
+};
